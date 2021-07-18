8cc8fd87aa7d6c57303c1b5718485b9bc88c374c
Andrey Kniazev <worldpwn@hotmail.com>
2021-07-18 11:41:37 +0200

    (add) scaffold simple vue app that will work as example

---
+++ example/renpy-converter-example/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Converter/>
+</template>
+
+<script>
+import Converter from './components/Converter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Converter
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ example/renpy-converter-example/src/components/Converter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    12
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Converter',
+  data () {
+    return {
+      jsons: []
+    }
+  },
+  methods: {
+    some () {
+      const a = 3
+      return a
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
