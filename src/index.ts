8d3baf9f86206c29f2c3c5f29de1374d50c89615
Andrey Kniazev <worldpwn@hotmail.com>
2021-07-18 11:12:20 +0200

    (lint) added eslint

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-export function download() {
-  const element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent('label start:'));
-  element.setAttribute('download', 'renpy-converter.rpy');
 
-  element.style.display = 'none';
-  document.body.appendChild(element);
+const content = `label start:
 
-  element.click();
+<breake>
 
-  document.body.removeChild(element);
+somemore`
+
+export function download () {
+  const element = document.createElement('a')
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content))
+
+  element.setAttribute('download', 'renpy-converter.rpy')
+
+  element.style.display = 'none'
+  document.body.appendChild(element)
+
+  element.click()
+
+  document.body.removeChild(element)
 }
 
-
-download();
+download()
