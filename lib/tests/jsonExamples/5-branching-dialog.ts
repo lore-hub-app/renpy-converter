055840ed9272d88034af97215c0bebccd1c77c53
Andrey Kniazev <worldpwn@hotmail.com>
2021-09-14 08:36:05 +0200

    update JSON example 5-branching-dialog.ts to correct one

--- lib/tests/jsonExamples/5-branching-dialog.ts
+++ lib/tests/jsonExamples/5-branching-dialog.ts
@@ -1,7 +1,7 @@
 const json = {
   dialog_id: "a79f2f0a-9219-4f4e-bfc4-0acdc25d683c",
   starting_node_id: "f29667eb-8334-4f7c-8e18-3ab62831d577",
-  player_versions: ["1.5"],
+  player_versions: ["2.0"],
   documents: [
     { id: "3893ecdf-eb15-46f1-ae8b-3001d611c03e", name: "Кристиан Орвуд" },
     { id: "1d45bcdc-7e32-4b57-9779-b924bbce93c1", name: "Оливер Вертран" },
@@ -10,6 +10,7 @@ const json = {
     {
       id: "bb920770-d479-4c34-a586-1c304950e221",
       next_node_id: "",
+      options: [],
       contents: [
         {
           id: "6a237366-02d6-4894-8cc9-716bf7b88e7e",
@@ -29,6 +30,7 @@ const json = {
     {
       id: "fabce5e6-587c-4b3f-bd42-2d3401e2f479",
       next_node_id: "",
+      options: [],
       contents: [
         {
           id: "a25a8305-2dfc-42e0-9a3e-452e32703eb4",
@@ -40,6 +42,16 @@ const json = {
     {
       id: "f29667eb-8334-4f7c-8e18-3ab62831d577",
       next_node_id: "",
+      options: [
+        {
+          next_dialog_node_id: "aba11ba2-3c8c-4c82-a7cd-e25b2f414d1e",
+          text: "— Я не хотел!",
+        },
+        {
+          next_dialog_node_id: "b8a78827-a5f4-427f-be0c-8e6db80244ce",
+          text: "— Ты заслужил этого!",
+        },
+      ],
       contents: [
         {
           id: "af6ab7e8-db73-4ae3-93f5-fe8205fefdb7",
@@ -59,6 +71,7 @@ const json = {
     {
       id: "b8a78827-a5f4-427f-be0c-8e6db80244ce",
       next_node_id: "fabce5e6-587c-4b3f-bd42-2d3401e2f479",
+      options: [],
       contents: [
         {
           id: "b9ffd2f3-9e00-429c-bf12-beea13fbc6e9",
@@ -78,6 +91,7 @@ const json = {
     {
       id: "aba11ba2-3c8c-4c82-a7cd-e25b2f414d1e",
       next_node_id: "bb920770-d479-4c34-a586-1c304950e221",
+      options: [],
       contents: [
         {
           id: "f2c40645-227a-4110-969d-5fbdf27644bb",
