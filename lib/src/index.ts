a9f7916471015966f994a71d2c5e83197556e1f0
Andrey Kniazev <worldpwn@hotmail.com>
2021-10-19 14:24:34 +0300

    change index export for lib

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -3,7 +3,7 @@ import getCharacters from "./internal/getCharacters";
 import getLoreHubJson from "./internal/getLoreHubJson";
 import getNodes from "./internal/getNodes";
 
-export default function convert(data: unknown) {
+export function convert(data: unknown) {
   const typedJson = getLoreHubJson(data);
   const nodes = getNodes(typedJson);
   const characters = getCharacters(typedJson.documents);
